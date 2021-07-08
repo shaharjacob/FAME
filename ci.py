0eb4edc0d812572c1a0633f39dc41553f6bf5d4b
huji520 <51635936+huji520@users.noreply.github.com>
2021-07-08 18:11:58 +0300

    ci tests

--- ci.py
+++ ci.py
@@ -1,20 +1,59 @@
 import unittest
 
-class TestStringMethods(unittest.TestCase):
+import concept_net
+import google_autosuggest
+from quasimodo import Quasimodo
 
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
 
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
+class TestFunctions(unittest.TestCase):
 
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
+    def test_concept_net(self):
+        # testing concept_net.get_entities_relations
+        reference = ['revolving around the']
+        actual = concept_net.get_entities_relations("earth", "sun")
+        self.assertEqual(sorted(reference), sorted(actual))
+
+        # testing concept_net.get_entity_props
+        reference = ['4.5 billion years old', 'a word', 'an oblate sphereoid', 'an oblate spheroid', 'finite', 'flat', 'one astronomical unit from the Sun', 'one of many planets', 'receive rain from clouds', 'revolving around the sun', 'round like a ball', 'spherical', 'spherical in shape', 'very beautiful', 'very heavy']
+        actual = concept_net.get_entity_props("earth", n_best=10)
+        self.assertEqual(sorted(reference), sorted(actual))
+
+
+    def test_google_autosuggest(self):
+        # testing google_autosuggest.get_entities_relations
+        reference = ['revolve around', 'move around the', 'rotate around', 'orbit', 'not fall into', 'circle the', 'revolve around', 'spin around the', 'rotate around', 'revolve around', 'move around', 'circle the', 'orbit']
+        actual = google_autosuggest.get_entities_relations("earth", "sun")
+        self.assertEqual(sorted(reference), sorted(actual))
+
+        # testing google_autosuggest.get_entity_props
+        reference = ['derived unit', 'fundamental unit', 'derived unit why', 'unit for measuring', 'unit of', 'fundamental unit or derived unit', 'measure of']
+        actual = google_autosuggest.get_entity_props("newton")
+        self.assertEqual(sorted(reference), sorted(actual))
+
+        # testing google_autosuggest.get_entity_suggestions
+        reference = ['edison', 'benjamin', 'faraday', 'they']
+        actual = google_autosuggest.get_entity_suggestions("electricity", "discovered")
+        self.assertEqual(sorted(reference), sorted(actual))
+
+
+    def test_quasimodo(self):
+        quasimodo = Quasimodo(path='tsv/quasimodo.tsv')
+        
+        # testing quasimodo.get_entity_props
+        reference = ['has body part hoof', 'eat grass', 'has body part leg', 'need horseshoes', 'has body part nose']
+        actual = [f"{prop[0]} {prop[1]}" for prop in quasimodo.get_entity_props('horse', n_largest=5, plural_and_singular=True)]
+        self.assertEqual(sorted(reference), sorted(actual))
+        
+        # testing quasimodo.get_entities_relations
+        reference = ['be to', 'rotate around', 'pull in', 'orbit', 'be closest star to']
+        actual = quasimodo.get_entities_relations('sun', 'earth', n_largest=5, plural_and_singular=True)
+        self.assertEqual(sorted(reference), sorted(actual))
+
+        # testing quasimodo.get_similarity_between_entities
+        reference = ['has temperature hot', 'has property aesthetic', 'has color blue', 'be in space', 'has property round']
+        actual = [f"{prop[0]} {prop[1]}" for prop in quasimodo.get_similarity_between_entities('sun', 'earth', n_largest=5, plural_and_singular=True)]
+        self.assertEqual(sorted(reference), sorted(actual))
+
 
 if __name__ == '__main__':
     unittest.main()
