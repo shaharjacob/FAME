f979606adb32ed502bb259d9269ce580a941943c
huji520 <51635936+huji520@users.noreply.github.com>
2021-07-29 22:12:39 +0300

    slpit evauation from tests

--- ci.py
+++ ci.py
@@ -3,12 +3,11 @@ import unittest
 from pathlib import Path
 
 import yaml
-from click import secho
 
 import concept_net
 import suggest_entities
 import google_autosuggest
-from mapping import mapping, get_all_possible_pairs_map, mapping_wrapper
+from mapping import mapping_wrapper
 from quasimodo import Quasimodo, merge_tsvs
 
 
@@ -108,36 +107,5 @@ class TestMapping(unittest.TestCase):
             self.assertEqual(round(reference, 3), round(actual, 3))
 
 
-def evaluate():
-    with open(TEST_FOLDER / 'evaluate.yaml', 'r') as y:
-        spec = yaml.load(y, Loader=yaml.SafeLoader)
-    mapping_spec = spec["mapping"]
-    total_good = 0
-    total_maps = 0
-    for tv in mapping_spec:
-
-        solutions = mapping_wrapper(base=tv["input"]["base"], target=tv["input"]["target"], suggestions=False, depth=tv["input"]["depth"], top_n=1)
-        solution = solutions[0]
-        current_good = 0
-        current_maps = 0
-
-        # check the mapping
-        actual = sorted(solution["mapping"])
-        reference = sorted(tv["output"]["mapping"])
-        for mapping in reference:
-            if mapping in actual:
-                current_good += 1
-            current_maps += 1
-                
-        total_good += current_good
-        total_maps += current_maps
-        
-        secho(f'Base: {tv["input"]["base"]}', fg="blue")
-        secho(f'Target: {tv["input"]["target"]}', fg="blue")
-        secho(f'Correct answers: {current_good}/{current_maps}\n', fg="cyan")
-    secho(f'Total: {total_good}/{total_maps}\n', fg="green")
-
-
 if __name__ == '__main__':
     unittest.main()
-    evaluate()
---
+++ evaluation.py
@@ -0,0 +1,42 @@
+from pathlib import Path
+
+import yaml
+from click import secho
+
+from mapping import mapping_wrapper
+
+
+TEST_FOLDER = Path('tests')
+
+def evaluate():
+    with open(TEST_FOLDER / 'evaluate.yaml', 'r') as y:
+        spec = yaml.load(y, Loader=yaml.SafeLoader)
+    mapping_spec = spec["mapping"]
+    total_good = 0
+    total_maps = 0
+    for tv in mapping_spec:
+
+        solutions = mapping_wrapper(base=tv["input"]["base"], target=tv["input"]["target"], suggestions=False, depth=tv["input"]["depth"], top_n=1)
+        solution = solutions[0]
+        current_good = 0
+        current_maps = 0
+
+        # check the mapping
+        actual = sorted(solution["mapping"])
+        reference = sorted(tv["output"]["mapping"])
+        for mapping in reference:
+            if mapping in actual:
+                current_good += 1
+            current_maps += 1
+                
+        total_good += current_good
+        total_maps += current_maps
+        
+        secho(f'Base: {tv["input"]["base"]}', fg="blue")
+        secho(f'Target: {tv["input"]["target"]}', fg="blue")
+        secho(f'Correct answers: {current_good}/{current_maps}\n', fg="cyan")
+    secho(f'Total: {total_good}/{total_maps}\n', fg="green")
+
+
+if __name__ == '__main__':
+    evaluate()
