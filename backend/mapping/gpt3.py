e05f4bb47b31455084dc9acd182c9dee29c83a4b
shaharjacob-mac <shahar.j12@gmail.com>
2022-05-28 16:12:54 +0300

    allow relations to be None in gpt3

--- backend/mapping/gpt3.py
+++ backend/mapping/gpt3.py
@@ -46,6 +46,12 @@ prompt = [
     "Q: What are the relations between water and pipe?",
     "A: Water flows through the pipe",
     "A: Water passes through the pipe",
+    "",
+    "Q: What are the relations between electron and wall?",
+    "A: None",
+    "",
+    "Q: What are the relations between water and basketball?",
+    "A: None"
     ""
 ]
 
@@ -122,42 +128,10 @@ def get_entities_relations_api(entity1: str, entity2: str):
         if response.choices and response.choices[0]:
             if "text" in response.choices[0]:
                 lines = response.choices[0]["text"].replace("A: ", "").split('\n')
-                relations = [line for line in lines if line]
+                relations = [line for line in lines if line and line != 'None']
 
     return relations
 
 
 if __name__ == "__main__":
-
-    # with open(DATABASE_FOLDER / 'gpt3_edges2.json', 'r') as f:
-    #     current_content = json.load(f)
-
-    with open(EVALUATION_FOLDER / 'green_eval_far_orig.yaml', 'r') as y:
-        spec = yaml.load(y, Loader=yaml.SafeLoader)
-    mapping_spec = spec["mapping"]
-    content = {}
-    engine = inflect.engine()
-    for mapping in tqdm(mapping_spec):
-        i = mapping['input']
-        for domain in ['base', 'target']:
-            entities = i[domain]
-            for direction in range(2):
-                e1 = entities[direction]
-                e2 = entities[(direction+1)%2]
-
-                relations = get_entities_relations(e1, e2, engine)
-                content[f"{e1}#{e2}"] = relations
-
-                # if f"{e1}#{e2}" in current_content:
-                #     continue
-
-                # relations = get_entities_relations_api(e1, e2)
-                # current_content[f"{e1}#{e2}"] = relations
-    
-                # with open(DATABASE_FOLDER / 'gpt3_edges2.json', 'w') as fw:
-                #     json.dump(current_content, fw, indent='\t')
-
-                # time.sleep(1)
-
-    with open(DATABASE_FOLDER / 'gpt_relations_both_direction.json', 'w') as fw:
-        json.dump(content, fw, indent='\t')
+    get_entities_relations_api("water tower", "hydrodynamics")
